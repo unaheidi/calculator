1a2f4537bcea9e1e86adec2e47df5246d806b234
Tommi <mato12@hotmail.fi>
2016-02-27 13:58:23 +0200

    init

---
+++ calculator.js
@@ -0,0 +1,74 @@
+if (Meteor.isClient) {
+    var display = 'display';
+    Session.setDefault(display, '');
+
+    Template.display.helpers({
+        placeholder: '1+2=3',
+        result: function () {
+            return Session.get(display);
+        }
+    });
+
+    Template.numpad.helpers({
+        buttons: function () {
+            return [
+                [
+                    {value: 'C', classType:'danger'},
+                    {value: '(', classType:'primary'},
+                    {value: ')', classType:'primary'},
+                    {value: '<', classType:'default'},
+                ],
+                [
+                    {value: '7', classType:'info'},
+                    {value: '8', classType:'info'},
+                    {value: '9', classType:'info'},
+                    {value: '/', classType:'primary'},
+                ],
+                [
+                    {value: '4', classType:'info'},
+                    {value: '5', classType:'info'},
+                    {value: '6', classType:'info'},
+                    {value: '*', classType:'primary'},
+                ],
+                [
+                    {value: '1', classType:'info'},
+                    {value: '2', classType:'info'},
+                    {value: '3', classType:'info'},
+                    {value: '-', classType:'primary'},
+                ],
+                [
+                    {value: '0', classType:'info'},
+                    {value: '.', classType:'info'},
+                    {value: '=', classType:'success'},
+                    {value: '+', classType:'primary'},
+                ],
+            ];
+        }
+    });
+
+    Template.button.events({
+        'click button': function (event) {
+            var buttonValue = event.target.value;
+            var output = Session.get(display);
+            if (isNaN(buttonValue)) {
+                if(buttonValue == "=") {
+                    Session.set(display, eval(output));
+                } else if (buttonValue == "C") {
+                    Session.set(display, '');
+                } else if (buttonValue == '<') {
+                    Session.set(display, output.slice(0, -1));
+                } else {
+                    Session.set(display, output + buttonValue);
+                }
+            } else {
+                Session.set(display, output + buttonValue);
+            }
+        }
+    });
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
+}
